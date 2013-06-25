7cff532712cf1f48f22e32a01deed2e05b813744
Dan Englesson <dan@dan-G53SW.(none)>
2013-06-25 06:58:23 +0200

    Setting up a simple SContruct file and a init main function.

---
+++ SConstruct
@@ -0,0 +1,1 @@
+Program('orka.cpp')
---
+++ orka.cpp
@@ -0,0 +1,10 @@
+/*
+ *
+ * orka.cpp
+ *
+ */
+#include <iostream>
+
+int main(int argc, char* argv[]){
+  std::cout << "This is a Linux image viewer!" << std::endl;
+}
