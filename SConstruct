7e4707b71484356734fc71b899b7020a0514ba50
Vilhelm Hedberg <vilhelmo@gmail.com>
2013-06-30 16:54:42 -0700

    Read a file using OpenImageIO.

--- SConstruct
+++ SConstruct
@@ -1,1 +1,21 @@
-Program('orka.cpp')
+
+env = Environment()
+
+
+#env.ParseConfig( 'pkg-config --cflags glib-2.0' )
+
+# Libraries:
+env.Append(CPPPATH = ['/usr/local/include/'])
+env.Append(LIBPATH = ['/usr/local/lib/'])
+env.Append(LIBS = ['OpenImageIO',])
+
+# Compile time flags.
+env.Append(CCFLAGS = ['-g','-O3'])
+
+# Linker flags
+env.Append(LINKFLAGS = ['-Wall'])
+
+### The program itself:
+o = env.Program(target='orka', source=['orka.cpp'])
+
+Default(o)
--- orka.cpp
+++ orka.cpp
@@ -4,7 +4,35 @@
  *
  */
 #include <iostream>
+#include <OpenImageIO/imageio.h>
+
+using namespace OpenImageIO;
 
 int main(int argc, char* argv[]){
-  std::cout << "This is a Linux image viewer!" << std::endl;
+	std::cout << "This is a Linux image viewer!" << std::endl;
+
+	if (argc < 2) {
+		std::cerr << "Need an image path." << std::endl;
+		return 1;
+	}
+
+	char * filename = argv[1];
+	std::cout << "Reading image: " << filename << std::endl;
+	ImageInput *in = ImageInput::open(filename);
+	if (!in) {
+		return 1;
+	}
+	const ImageSpec &spec = in->spec();
+	int xres = spec.width;
+	int yres = spec.height;
+	int channels = spec.nchannels;
+	std::vector<unsigned char> pixels (xres*yres*channels);
+	in->read_image(TypeDesc::UINT8, &pixels[0]);
+	in->close();
+
+	std::cout << "Image size " << xres << "x" << yres << std::endl;
+	std::cout << channels << " channels" << std::endl;
+
+
+	delete in;
 }
